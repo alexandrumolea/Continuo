c911379ccea87f06e6bc31a514d1b272c41d6b01
Alexandru Molea <alexandru.molea@gmail.com>
2026-05-23 19:17:09 +0300

    Initial Commit

---
+++ Continuo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Continuo
+//
+//  Created by Alexandru Molea on 5/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Continuo/ContinuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuoApp.swift
+//  Continuo
+//
+//  Created by Alexandru Molea on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ContinuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
